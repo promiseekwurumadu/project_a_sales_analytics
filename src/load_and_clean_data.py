b3badd21b7191c4658fc8423cce447b517fd0032
promiseekwurumadu <promiseekwurumadu@gmail.com>
2025-12-30 09:02:07 +0000

    Update load_and_clean_data.py

--- src/load_and_clean_data.py
+++ src/load_and_clean_data.py
@@ -1,1 +1,17 @@
+import pandas as pd
+
+def load_data(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path)
+    return df
+
+def basic_cleaning(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.dropna(subset=["CustomerID"])
+    df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"])
+    df["Revenue"] = df["Quantity"] * df["UnitPrice"]
+    return df
+
+if __name__ == "__main__":
+    df = load_data("data/online_retail.csv")
+    clean_df = basic_cleaning(df)
+    print(clean_df.head())
 
